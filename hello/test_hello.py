3daf25f105f2a835e2f437bf68c2faa7e27fb2b4
Ram Basnet <rambasnet@gmail.com>
2023-09-23 17:31:21 -0600

    add hello to hello branch

---
+++ hello/test_hello.py
@@ -0,0 +1,10 @@
+"""Test module for hello.py 
+"""
+
+import unittest
+from hello import answer
+
+
+def test_answer() -> None:
+    """Test answer() function."""
+    assert answer() == "Hello World!"
